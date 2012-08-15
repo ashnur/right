130433744689300336665fa65171b265065f4aa7
Ashnur <ashnur@gmail.com>
2012-08-15 12:41:07 +0300

    clear up

--- test/test.js
+++ test/test.js
@@ -1,22 +1,22 @@
-"use strict";
-
-describe("right", function() {
-  var expect, nmbr, right;
-  right = require(__dirname + '/../right.js');
-  nmbr = require(__dirname + '/../node_modules/nmbr/index.js');
-  expect = require('expect.js');
-  it("should be a function", function() {
-    return expect(right).to.be.an('function');
-  });
-  return it('should return nmbr objects after parsing strings', function() {
-    var one, r, two;
-    one = nmbr(1);
-    two = nmbr(2);
-    r = right('1');
-    expect(one).to.be(r);
-    r = right('+1');
-    expect(one).to.be(r);
-    r = right('2');
-    return expect(two).to.be(r);
-  });
-});
+"use strict"
+describe("right", function(){
+    var expect, rat, right
+        ;
+    right = require('../right.js')
+    rat = require('rats')
+    expect = require('expect.js')
+    it("should be a function", function() {
+        return expect(right).to.be.an('function')
+    })
+    return it('should return rat objects after parsing strings', function() {
+        var one, r, two
+        one = rat(1)
+        two = rat(2)
+        r = right('1')
+        expect(one).to.be(r)
+        r = right('+1')
+        expect(one).to.be(r)
+        r = right('2')
+        return expect(two).to.be(r)
+    })
+})
